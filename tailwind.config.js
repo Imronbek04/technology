4631589238df1bede22e932226be0445bd3331af
Imronbek04 <tursunovimronbek444@gmail.com>
2023-09-30 22:15:28 +0500

    technology

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        'max': '1345px'
+      },
+      lineHeight: {
+        '60': '60px'
+      },
+      gap: {
+        '74': '74'
+      },
+      backgroundColor:{
+        'lightgrey' : '#F0F0F0'
+      }
+    },
+  },
+  plugins: [],
+}
+
